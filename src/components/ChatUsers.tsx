b21c78d58fe65bdd7821491de3c72094f09bc72b
Mohd-Muneeb <mohdmuneeb1307@gmail.com>
2022-09-19 00:43:39 +0530

    Adding ability to switch chat topics

--- src/components/ChatUsers.tsx
+++ src/components/ChatUsers.tsx
@@ -1,96 +1,3 @@
-// import React, { useState } from 'react';
-// import { collection, query, where, getDocs } from 'firebase/firestore';
-// import { db } from './app';
-// import UserCard from './UserCard';
-
-// export default function ChatUsers(props: any) {
-//     const [UserSearch, setUserSearch] = useState('');
-//     const [SearchResults, setSearchResults] = useState({});
-//     const [FoundResult, setFoundResult] = useState(false);
-//     const handleChange = async () => {
-//         const q = query(
-//             collection(db, 'Users'),
-//             where('username', '==', UserSearch)
-//         );
-
-//         const querySnapshot = await getDocs(q);
-//         querySnapshot.forEach((doc) => {
-//             // doc.data() is never undefined for query doc snapshots
-//             setSearchResults((prev) => [doc.data()]);
-//             console.log(SearchResults);
-//             setFoundResult(true);
-//             console.log(doc.id, ' => ', doc.data());
-//         });
-//     };
-
-//     return (
-//         <div className="bg-slate-600 w-[35%]">
-//             <div className="flex bg-slate-400 p-4 h-[10%]">
-//                 <div className="flex">
-//                     <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 rounded-l-md border border-r-0 border-gray-300 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
-//                         @
-//                     </span>
-//                     <input
-//                         type="text"
-//                         onKeyDown={handleChange}
-//                         onChange={(e) => {
-//                             setUserSearch(e.target.value);
-//                             if (e.target.value == '') setFoundResult(false);
-//                         }}
-//                         id="website-admin"
-//                         className="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-//                         placeholder="Username"
-//                     />
-//                 </div>
-//             </div>
-//             {/* {!FoundResult ? (
-//                 <ShowUsers />
-//             ) : (
-//                 <ShowSearchResults search={SearchResults} />
-//             )} */}
-//         </div>
-//     );
-// }
-
-// // function ShowUsers() {
-// //     return (
-// //         <div>
-// //             <UserCard
-// //                 Profile="http://dummyimage.com/219x110.png/cc0000/ffffff"
-// //                 UserName="testing1"
-// //                 FirstName="Lmao"
-// //             />
-// //             <UserCard
-// //                 Profile="http://dummyimage.com/213x234.png/ff4444/ffffff"
-// //                 UserName="testing2"
-// //                 FirstName="Bruh"
-// //             />
-// //             <UserCard
-// //                 Profile="http://dummyimage.com/249x132.png/5fa2dd/fffffff"
-// //                 UserName="testing3"
-// //                 FirstName="Sheesh"
-// //             />
-// //         </div>
-// //     );
-// // }
-
-// // function ShowSearchResults(props: any) {
-// //     console.log(props.search);
-// //     return (
-// //         <div className='p-4 bg-slate-600' >
-// //             {props.search ? (
-// //                 <UserCard
-// //                     Profile={props.search[0].profile}
-// //                     UserName={props.search[0].username}
-// //                     FirstName={props.search[0].Name}
-// //                 />
-// //             ) : (
-// //                 <h1>Loading</h1>
-// //             )}
-// //         </div>
-// //     );
-// // }
-
 import { query } from 'firebase/database';
 import {
     addDoc,
@@ -164,7 +71,7 @@ function ChatRoom(props: any) {
                 />
 
                 <button type="submit" disabled={!formValue}>
-                    🕊️
+                    Send
                 </button>
             </form>
         </>
@@ -178,11 +85,12 @@ function ChatMessage(props: any) {
 
     return (
         <>
-            <div className={`message ${messageClass}`}>
+            <div className={`message flex ${messageClass}`}>
                 <img
+                    className="w-8 h-8 mr-4 rounded-full"
                     src={
                         photoURL ||
-                        'https://api.adorable.io/avatars/23/abott@adorable.png'
+                        'https://media.istockphoto.com/vectors/missing-image-of-a-person-placeholder-vector-id1288129985?k=20&m=1288129985&s=612x612&w=0&h=OHfZHfKj0oqIDMl5f_oRqH13MHiB63nUmySYILbWbjE='
                     }
                 />
                 <p>{text}</p>
--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -3,51 +3,46 @@ import ChatUsers from './ChatUsers';
 
 export default function Messages() {
     const [ActiveTab, setActiveTab] = useState('');
+
     return (
-        <div className="w-[100%] h-screen bg-slate-500">
+        <div className="w-[100%] h-screen bg-slate-800">
             <div className="h-[10%] bg-slate-800 text-xl justify-start px-12 flex items-center">
                 <div className="overflow-x-auto flex w-[70%]">
                     <ul className="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400">
                         <li className="mr-2">
-                            <button
-                                onClick={() => setActiveTab('Anime')}
-                                className="inline-block py-3 px-4 text-white bg-blue-600 rounded-lg active"
-                                aria-current="page"
-                            >
-                                Anime
-                            </button>
+                            <Button
+                                IsActive={ActiveTab}
+                                setActiveTab={setActiveTab}
+                                Name="Anime"
+                            />
                         </li>
                         <li className="mr-2">
-                            <button
-                                onClick={() => setActiveTab('Coding')}
-                                className="inline-block py-3 px-4 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white"
-                            >
-                                Coding
-                            </button>
+                            <Button
+                                IsActive={ActiveTab}
+                                setActiveTab={setActiveTab}
+                                Name="Coding"
+                            />
                         </li>
                         <li className="mr-2">
-                            <button
-                                onClick={() => setActiveTab('Gaming')}
-                                className="inline-block py-3 px-4 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white"
-                            >
-                                Gaming
-                            </button>
+                            <Button
+                                IsActive={ActiveTab}
+                                setActiveTab={setActiveTab}
+                                Name="Gaming"
+                            />
                         </li>
                         <li className="mr-2">
-                            <button
-                                onClick={() => setActiveTab('VideoEditing')}
-                                className="inline-block py-3 px-4 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white"
-                            >
-                                Video Editing
-                            </button>
+                            <Button
+                                IsActive={ActiveTab}
+                                setActiveTab={setActiveTab}
+                                Name="Video Editing"
+                            />
                         </li>
                         <li className="mr-2">
-                            <button
-                                onClick={() => setActiveTab('Sports')}
-                                className="inline-block py-3 px-4 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white"
-                            >
-                                Sports
-                            </button>
+                            <Button
+                                IsActive={ActiveTab}
+                                setActiveTab={setActiveTab}
+                                Name="Sports"
+                            />
                         </li>
                     </ul>
                 </div>
@@ -57,9 +52,32 @@ export default function Messages() {
                 />
                 <h1 className="text-white">@Muneeb</h1>
             </div>
-            <div className="bg-amber-100 h-[90%]">
-                <ChatUsers active={ActiveTab} />
+            <div className="bg-slate-400 h-[90%]">
+                {ActiveTab == '' ? (
+                    <h1>Select something</h1>
+                ) : (
+                    <ChatUsers active={ActiveTab} />
+                )}
             </div>
         </div>
+    );
+}
+
+function Button(props: any) {
+    return props.IsActive == props.Name ? (
+        <button
+            onClick={() => props.setActiveTab(props.Name)}
+            className="inline-block py-3 px-4 bg-blue-700 text-white rounded-lg active"
+            aria-current="page"
+        >
+            {props.Name}
+        </button>
+    ) : (
+        <button
+            onClick={() => props.setActiveTab(props.Name)}
+            className="inline-block py-3 px-4 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white"
+        >
+            {props.Name}
+        </button>
     );
 }
